a2276f9a18cfc20ce367a3728535544c2bec05a3
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-03-21 21:58:17 +0000

    add view engine ejs, add more urls, add urls_index

--- express_server.js
+++ express_server.js
@@ -6,11 +6,19 @@ app.set("view engine", "ejs")
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  "q64A7G": "https://na.finalfantasy.com",
+  "loHenP": "http://iro.ragnarokonline.com",
+  "SuplrX": "https://www.playlostark.com",
+  "OAQJtK": "https://www.kingdomhearts.com"
 };
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  res.render("urls_index", { urls: urlDatabase });
 });
 
 app.get("/urls.json", (req, res) => {
