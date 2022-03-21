4e3bbcb6548c24b626c45b5f6b9264662723b052
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-03-21 22:06:42 +0000

    add page to show single url and destination

--- express_server.js
+++ express_server.js
@@ -18,7 +18,16 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  res.render("urls_index", { urls: urlDatabase });
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+  };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
