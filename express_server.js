07fed82b26eecb5d73319e4ecb52d6e6f7791a81
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-03-22 02:47:01 +0000

    install cookies package

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const cookieParser = require('cookie-parser')
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
